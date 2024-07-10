05c54d028bd8ed03f865bce2e2b28077ea9d0bfb
Oliver Bähler <oliverbaehler@hotmail.com>
2024-07-10 14:55:32 +0200

    feat(manager): add sveltos manager
    Signed-off-by: Oliver Bähler <oliverbaehler@hotmail.com>

--- pkg/controller/user-cluster-controller-manager/flatcar/controller.go
+++ pkg/controller/user-cluster-controller-manager/flatcar/controller.go
@@ -26,8 +26,10 @@ import (
 	nodelabelerapi "k8c.io/kubermatic/v2/pkg/controller/user-cluster-controller-manager/node-labeler/api"
 	controllerutil "k8c.io/kubermatic/v2/pkg/controller/util"
 	predicateutil "k8c.io/kubermatic/v2/pkg/controller/util/predicate"
+	"k8c.io/kubermatic/v2/pkg/provider/kubernetes"
 	"k8c.io/kubermatic/v2/pkg/resources/registry"
 	"k8c.io/reconciler/pkg/reconciling"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -45,18 +47,27 @@ const (
 
 type Reconciler struct {
 	ctrlruntimeclient.Client
+	seedClient        ctrlruntimeclient.Client
+	namespace         string
+	clusterIsPaused   userclustercontrollermanager.IsPausedChecker
 	overwriteRegistry string
 	updateWindow      kubermaticv1.UpdateWindow
-	clusterIsPaused   userclustercontrollermanager.IsPausedChecker
+	networkPolices    bool
+	cache             cache.Cache
 }
 
-func Add(mgr manager.Manager, overwriteRegistry string, updateWindow kubermaticv1.UpdateWindow, clusterIsPaused userclustercontrollermanager.IsPausedChecker) error {
+func Add(mgr manager.Manager, seedMgr manager.Manager, overwriteRegistry string, updateWindow kubermaticv1.UpdateWindow, clusterIsPaused userclustercontrollermanager.IsPausedChecker, networkPolices bool) error {
 	reconciler := &Reconciler{
 		Client:            mgr.GetClient(),
 		overwriteRegistry: overwriteRegistry,
 		updateWindow:      updateWindow,
 		clusterIsPaused:   clusterIsPaused,
+		networkPolices:    networkPolices,
 	}
+
+	reconciler.Client = mgr.GetClient()
+	reconciler.seedClient = seedMgr.GetClient()
+	reconciler.cache = mgr.GetCache()
 
 	_, err := builder.ControllerManagedBy(mgr).
 		Named(ControllerName).
@@ -73,6 +84,23 @@ func (r *Reconciler) Reconcile(ctx context.Context, _ reconcile.Request) (reconc
 	}
 	if paused {
 		return reconcile.Result{}, nil
+	}
+
+	cluster, err := r.getCluster(ctx)
+	if err != nil {
+		return reconcile.Result{}, fmt.Errorf("failed to retrieve cluster: %w", err)
+	}
+
+	data := reconcileData{
+		caCert:       caCert,
+		userSSHKeys:  userSSHKeys,
+		ccmMigration: r.ccmMigration || r.ccmMigrationCompleted,
+		cluster:      cluster,
+	}
+
+	err = r.setupNetworkingData(cluster, &data)
+	if err != nil {
+		return reconcile.Result{}, fmt.Errorf("failed to setup cluster networking data: %w", err)
 	}
 
 	var nodeList corev1.NodeList
@@ -148,5 +176,51 @@ func (r *Reconciler) reconcileUpdateOperatorResources(ctx context.Context) error
 		return fmt.Errorf("failed to reconcile the DaemonSets: %w", err)
 	}
 
+	if r.networkPolices {
+		netReconcilers := []reconciling.NamedNetworkPolicyReconcilerFactory{
+			resources.NetworkPolicyReconciler(data),
+		}
+		if err := reconciling.ReconcileNetworkPolicies(ctx, netReconcilers, metav1.NamespaceSystem, r.Client); err != nil {
+			return fmt.Errorf("failed to reconcile the NetworkPolices: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func (r *Reconciler) getCluster(ctx context.Context) (*kubermaticv1.Cluster, error) {
+	cluster, err := kubernetes.ClusterFromNamespace(ctx, r.seedClient, r.namespace)
+	if err != nil {
+		return nil, err
+	}
+	if cluster == nil {
+		return nil, fmt.Errorf("no cluster exists for namespace %q", r.namespace)
+	}
+
+	return cluster, nil
+}
+
+func (r *Reconciler) setupNetworkingData(cluster *kubermaticv1.Cluster, data *reconcileData) (err error) {
+	data.k8sServiceApiIP, err = resources.InClusterApiserverIP(cluster)
+	if err != nil {
+		return fmt.Errorf("failed to get Cluster Apiserver IP: %w", err)
+	}
+
+	// Reconcile kubernetes service endpoints, unless it is not supported or disabled in the apiserver override settings.
+	data.reconcileK8sSvcEndpoints = true
+
+	if cluster.Spec.ComponentsOverride.Apiserver.EndpointReconcilingDisabled != nil && *cluster.Spec.ComponentsOverride.Apiserver.EndpointReconcilingDisabled {
+		// Do not reconcile if explicitly disabled.
+		data.reconcileK8sSvcEndpoints = false
+	}
+
+	if cluster.Spec.ExposeStrategy == kubermaticv1.ExposeStrategyTunneling {
+		data.k8sServiceEndpointAddress = r.tunnelingAgentIP.String()
+		data.k8sServiceEndpointPort = int32(r.kasSecurePort)
+	} else {
+		data.k8sServiceEndpointAddress = cluster.Status.Address.IP
+		data.k8sServiceEndpointPort = cluster.Status.Address.Port
+	}
+	data.ipFamily = cluster.Spec.ClusterNetwork.IPFamily
 	return nil
 }
---
+++ pkg/controller/user-cluster-controller-manager/flatcar/resources/networkpolicy.go
@@ -0,0 +1,78 @@
+/*
+Copyright 2022 The Kubermatic Kubernetes Platform contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package resources
+
+import (
+	"fmt"
+
+	"k8c.io/reconciler/pkg/reconciling"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+)
+
+// NetworkPolicyReconciler NetworkPolicy allows egress traffic of user ssh keys agent to the world.
+func NetworkPolicyReconciler(k8sApiIP string, k8sApiPort int, k8sServiceApi string) reconciling.NamedNetworkPolicyReconcilerFactory {
+	return func() (string, reconciling.NetworkPolicyReconciler) {
+		apiServicePort := intstr.FromInt(443)
+		apiPort := intstr.FromInt(k8sApiPort)
+		protoTcp := corev1.ProtocolTCP
+
+		return "user-ssh-key-agent", func(np *networkingv1.NetworkPolicy) (*networkingv1.NetworkPolicy, error) {
+			np.Spec = networkingv1.NetworkPolicySpec{
+				PodSelector: metav1.LabelSelector{
+					MatchLabels: map[string]string{"app.kubernetes.io/name": OperatorDeploymentName},
+				},
+				PolicyTypes: []networkingv1.PolicyType{
+					networkingv1.PolicyTypeEgress,
+				},
+				Egress: []networkingv1.NetworkPolicyEgressRule{
+					{
+						To: []networkingv1.NetworkPolicyPeer{
+							{
+								IPBlock: &networkingv1.IPBlock{
+									CIDR: fmt.Sprintf("%s/32", k8sApiIP),
+								},
+							},
+						},
+						Ports: []networkingv1.NetworkPolicyPort{
+							{
+								Port:     &apiPort,
+								Protocol: &protoTcp,
+							},
+						},
+					},
+					{
+						To: []networkingv1.NetworkPolicyPeer{
+							{
+								IPBlock: &networkingv1.IPBlock{
+									CIDR: fmt.Sprintf("%s/32", k8sServiceApi),
+								},
+							},
+						},
+						Ports: []networkingv1.NetworkPolicyPort{
+							{
+								Port:     &apiServicePort,
+								Protocol: &protoTcp,
+							},
+						},
+					},
+				},
+			}
+			return np, nil
+		}
+	}
+}
